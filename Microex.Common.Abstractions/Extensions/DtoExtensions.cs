3fc37e5d51291d9dd5d751b67b756628f7ce45d1
snys98 <snys98@outlook.com>
2017-12-03 18:02:54 +0800

    update dto abstractions

---
+++ Microex.Common.Abstractions/Extensions/DtoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microex.Common.Abstractions.Extensions
+{
+    public static class DtoExtensions
+    {
+        public static void SetStatus(this IResponse @this, int code,string message = "")
+        {
+            @this.Status = new ResponseStatus() {Code = code, Message = message};
+        }
+    }
+}
